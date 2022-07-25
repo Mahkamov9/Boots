b15df1a3b3317a4fecacbb8db93f9fe262a8f73d
Mahkamov9 <ahadjonmahkamov111@gmail.com>
2022-07-25 12:18:57 +0500

    sx

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
 const menu = document.getElementById('menu-icon')
 const menuModal = document.getElementById('menu-modal')
 
-menu.addEventListener("click", function(e){
-  menuModal.style.diplay="block"
-
-
-})
-
-console.log('Salom');
