dfceb43d7de7863dd3ad39db1daa3e31ba7f12bf
Mahkamov9 <ahadjonmahkamov111@gmail.com>
2022-07-25 10:24:22 +0500

    EAFA

---
+++ main.js
@@ -0,0 +1,10 @@
+const menu = document.getElementById('menu-icon')
+const menuModal = document.getElementById('menu-modal')
+
+menu.addEventListener("click", function(e){
+  menuModal.style.diplay="block"
+
+
+})
+
+console.log('Salom');
